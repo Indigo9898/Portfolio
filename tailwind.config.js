8ebe399e648d8e8d53f9ecbf3d6b6a2078839a56
Tyler <adyer1717@gmail.com>
2022-09-12 12:45:50 -0500

    miss capitalization in tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     minHeight: {
